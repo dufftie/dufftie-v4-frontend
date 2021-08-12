359300b54916c56075ee4a50908fe2fb5839d0c8
dufftie <dufftie@gmail.com>
2021-08-12 15:42:50 +0300

    Nuxt project init
    -Created project
    -Configured basic things
    -Created sample application for creators

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav id="navbar">
+      <div class="navbar-logo-container">
+        <div class="navbar-logo">
+          <a href="/"><span class="navbar-logo-text" id="homeButton">Dufftie Design</span></a>
+          <img src="~/static/logo_x1080.png" alt="logoBg" class="navbar-logo-img">
+        </div>
+      </div>
+      <div class="navbar-links-container">
+        <div class="navbar-links-scroll">
+          <a href="/everything">Design</a>
+          <a href="/more">More</a>
+          <a href="/about">About</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  import "~/assets/styles/header.css";
+  export default {
+  }
+</script>
+
